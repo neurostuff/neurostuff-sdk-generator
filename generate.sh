c18d2a15ac080da74bac552816abc77fc820814e
James Kent <jamesdkent21@gmail.com>
2022-04-01 15:55:46 -0500

    update generation code

--- generate.sh
+++ generate.sh
@@ -4,6 +4,14 @@ docker run --rm  --user $(id -u):$(id -g) \
     -v $PWD:/local openapitools/openapi-generator-cli:v5.4.0 generate \
     -i /local/neurostore-spec/neurostore-openapi.yml \
     -g python \
-    -c /local/api_config.json \
-    -o /local/neurostore-sdk \
+    -c /local/neurostore_python_sdk_config.json \
+    -o /local/python/neurostore-sdk \
+    --global-property debugOperations > debug.txt
+
+docker run --rm  --user $(id -u):$(id -g) \
+    -v $PWD:/local openapitools/openapi-generator-cli:v5.4.0 generate \
+    -i /local/neurostore-spec/analysis-specification.yml \
+    -g python \
+    -c /local/neurosynth_compose_python_sdk_config.json \
+    -o /local/python/neurosynth-compose-sdk \
     --global-property debugOperations > debug.txt
