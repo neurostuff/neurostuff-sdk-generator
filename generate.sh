3027ae7441d72393cce48be48f52ddb501955009
James Kent <jamesdkent21@gmail.com>
2023-03-16 12:10:51 -0500

    update configs to new generator

--- generate.sh
+++ generate.sh
@@ -2,17 +2,6 @@
 
 generate=$1
 
-if [ -z ${generate} ] || [ ${generate} == "python-neurostore" ] || [ ${generate} == "neurostore-python-sdk" ]; then
-
-    echo "generating python-neurostore-sdk..."
-    docker run --rm  --user $(id -u):$(id -g) \
-        -v $PWD:/local openapitools/openapi-generator-cli:v5.4.0 generate \
-        -i /local/neurostore-spec/neurostore-openapi.yml \
-        -g python \
-        -c /local/neurostore_python_sdk_config.json \
-        -o /local/python/neurostore-python-sdk
-fi
-
 if [ -z ${generate} ] || [ ${generate} == "typescript-neurostore" ] || [ ${generate} == "neurostore-typescript-sdk" ]; then
 
     echo "generating typescript-neurostore-sdk..."
@@ -21,17 +10,6 @@ if [ -z ${generate} ] || [ ${generate} == "typescript-neurostore" ] || [ ${gener
         -i /local/neurostore-spec/neurostore-openapi.yml \
         -g typescript-axios \
         -o /local/typescript/neurostore-typescript-sdk
-fi
-
-if [ -z ${generate} ] || [ ${generate} == "python-neurosynth-compose" ] || [ ${generate} == "neurosynth-compose-python-sdk" ]; then
-
-    echo "generating python-neurosynth-compose-sdk..."
-    docker run --rm  --user $(id -u):$(id -g) \
-        -v $PWD:/local openapitools/openapi-generator-cli:v5.4.0 generate \
-        -i /local/neurostore-spec/neurosynth-compose-openapi.yml \
-        -g python \
-        -c /local/neurosynth_compose_python_sdk_config.json \
-        -o /local/python/neurosynth-compose-python-sdk
 fi
 
 if [ -z ${generate} ] || [ ${generate} == "typescript-neurosynth-compose" ] || [ ${generate} == "neurosynth-compose-typescript-sdk" ]; then
