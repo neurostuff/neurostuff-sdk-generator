40e1ee4e68a5bdff900d12883b1eb7a36cb0e872
James Kent <jamesdkent21@gmail.com>
2023-05-29 14:24:01 -0500

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,9 +6,9 @@ if [ -z ${generate} ] || [ ${generate} == "python-neurostore" ] || [ ${generate}
 
     echo "generating python-neurostore-sdk..."
     docker run --rm  --user $(id -u):$(id -g) \
-        -v $PWD:/local openapitools/openapi-generator-cli:v6.6.0 generate \
+        -v $PWD:/local openapitools/openapi-generator-cli:706452c26389 generate \
         -i /local/neurostore-spec/neurostore-openapi.yml \
-        -g python-nextgen \
+        -g python \
         -c /local/neurostore_python_nextgen_sdk_config.json \
         -o /local/python/neurostore-python-sdk
 fi
@@ -27,9 +27,9 @@ if [ -z ${generate} ] || [ ${generate} == "python-neurosynth-compose" ] || [ ${g
 
     echo "generating python-neurosynth-compose-sdk..."
     docker run --rm  --user $(id -u):$(id -g) \
-        -v $PWD:/local openapitools/openapi-generator-cli:v6.6.0 generate \
+        -v $PWD:/local openapitools/openapi-generator-cli:706452c26389 generate \
         -i /local/neurostore-spec/neurosynth-compose-openapi.yml \
-        -g python-nextgen \
+        -g python \
         -c /local/neurosynth_compose_python_nextgen_sdk_config.json \
         -o /local/python/neurosynth-compose-python-sdk
 fi
